12cf4cf10291de614dc01c1253bc2ef6f053f98b
aabhapingle <61047894+aabhapingle@users.noreply.github.com>
2020-05-10 11:49:46 +0530

    Changed port

--- app.js
+++ app.js
@@ -114,6 +114,6 @@ app.get('/contactus', function(req , res){
 // 	res.render('index')
 // })
 
-app.listen(3000 , function(){
+app.listen(process.env.PORT || 3000 , function(){
 	console.log('The ngo2 server has started.');
 });
